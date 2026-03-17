1af523f9887a1f8b949e3e48e609bb1248829052
yriyriyri <oscar.paterson93@gmail.com>
2026-03-17 21:46:43 +0900

    start script changed

--- start/start-kidcup.bat
+++ start/start-kidcup.bat
@@ -3,21 +3,10 @@ setlocal
 
 cd /d "%USERPROFILE%\Desktop\kidcup"
 
-echo Pulling latest changes...
-git pull
-
-echo Installing dependencies...
-call npm install
-
-echo Building project...
-call npm run build
-
-echo Starting server...
 start "Kidcup Server" cmd /k "cd /d %USERPROFILE%\Desktop\kidcup && npm run start"
 
 timeout /t 5 /nobreak >nul
 
-echo Opening Chrome...
-start chrome http://localhost:3000
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" http://localhost:3000
 
 endlocal
